1c9c0f587c8350ba3e871e06587737c0a5f9f6d9
Koushik Dey <89943976+koushik80@users.noreply.github.com>
2022-03-07 01:54:37 +0200

    Update Messenger.jsx

--- client/src/pages/messenger/Messenger.jsx
+++ client/src/pages/messenger/Messenger.jsx
@@ -20,7 +20,7 @@ export default function Messenger() {
   const scrollRef = useRef();
 
   useEffect(() => {
-    socket.current = io("ws://localhost:8900");
+    socket.current = io("https://devhub-message.herokuapp.com");
     socket.current.on("getMessage", (data) => {
       setArrivalMessage({
         sender: data.senderId,
